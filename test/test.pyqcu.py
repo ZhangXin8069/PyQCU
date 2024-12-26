9bdaeeaf463d68869f70b60131470a0676d8ab02
zhangxin <zhangxin8069@qq.com>
2024-12-26 14:25:54 +0000

    2024-12-26-14-25-54

--- test/test.pyqcu.py
+++ test/test.pyqcu.py
@@ -1,29 +1,41 @@
+import ctypes
 from pyqcu import qcu
 import cupy as cp
 import numpy as np
 import re
-filename = "wilson-bistabcg-gauge_-32-32-32-32-1048576-1-1-1-1-0-0-1-0-f.bin"
+gauge_filename = "wilson-bistabcg-gauge_-32-32-32-32-1048576-1-1-1-1-0-0-1-0-f.bin"
 pattern = r"-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-"
-match = re.search(pattern, filename)
+match = re.search(pattern, gauge_filename)
 if match:
     param = [int(num) for num in match.groups()]
     print("Extracted integers:", param)
-    params = np.array(param, dtype=int)
-    argv = cp.array(params, dtype=cp.double)
-    print("NumPy Array:", params)
+    py_params = np.array(param, dtype=int)
+    py_argv = np.array(py_params, dtype=np.float32)
+    params = py_params.ctypes.data
+    argv = py_argv.ctypes.data
+    print(params)
+    print(type(params))
+    print("NumPy Array:", py_params)
     _LAT_XYZT_ = 4
     _LAT_DCC_ = 36
     _LAT_SC_ = 12
     _EVEN_ODD = 2
-    size = params[_LAT_XYZT_]*_LAT_DCC_
-    gauge = cp.fromfile(filename, dtype=cp.complex64, count=size)
-    filename.replace("gauge", "fermion-in")
-    size = params[_LAT_XYZT_]*_LAT_SC_
-    fermion_in = cp.fromfile(filename, dtype=cp.complex64, count=size)
-    filename.replace("fermion-in", "fermion-out")
-    fermion_out = cp.fromfile(filename, dtype=cp.complex64, count=size)
+    size = py_params[_LAT_XYZT_]*_LAT_DCC_
+    gauge_filename = gauge_filename.replace("gauge", "gauge")
+    py_gauge = cp.fromfile(gauge_filename, dtype=cp.complex64, count=size)
+    gauge = py_gauge.data.ptr
+    size = py_params[_LAT_XYZT_]*_LAT_SC_
+    fermion_in_filename = gauge_filename.replace("gauge", "fermion-in")
+    py_fermion_in = cp.fromfile(
+        fermion_in_filename, dtype=cp.complex64, count=size)
+    fermion_in = py_fermion_in.data.ptr
+    fermion_out_filename = gauge_filename.replace("gauge", "fermion-out")
+    py_fermion_out = cp.fromfile(
+        fermion_out_filename, dtype=cp.complex64, count=size)
+    fermion_out = py_fermion_out.data.ptr
+    print(gauge)
     qcu.applyBistabCgQcu(fermion_out, fermion_in, gauge, params, argv)
+    # qcu.applyBistabCgQcu(py_fermion_out, py_fermion_in, py_gauge, py_params, py_argv)
 else:
     print("No match found!")
     exit(1)
-
--- test/test.void.py
+++ test/test.void.py
@@ -2,19 +2,31 @@ import numpy as np
 import cupy as cp
 import ctypes
 
-# 从NumPy数组获取void*指针
-numpy_array = np.array([1, 2, 3], dtype=np.int32)
-numpy_ptr = numpy_array.ctypes.data
-print(type(numpy_array))
-print(type(numpy_ptr))
-numpy_void_ptr = ctypes.c_void_p(numpy_ptr)
-print(type(numpy_void_ptr))
-print(numpy_void_ptr)
-# 从CuPy数组获取void*指针
-cupy_array = cp.array([1, 2, 3], dtype=cp.int32)
-cupy_ptr = cupy_array.data.ptr
-print(type(cupy_array))
-print(type(cupy_ptr))
-cupy_void_ptr = ctypes.c_void_p(cupy_ptr)
-print(type(cupy_void_ptr))
-print(cupy_void_ptr)
+# 创建一个 numpy 数组
+arr = np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32)
+
+# 获取数据的 ctypes 指针
+data_ptr = arr.ctypes.data
+print("Numpy data pointer:", data_ptr)
+print("type:", type(data_ptr))
+# 获取指针的原始地址，并使用 c_void_p 包装
+void_ptr = ctypes.c_void_p(data_ptr)
+print("Wrapped void pointer:", void_ptr)
+print("type:", type(void_ptr))
+print("Numpy data pointer:", data_ptr)
+print("Wrapped void pointer:", void_ptr)
+
+print("###################")
+# 创建一个 numpy 数组
+arr = cp.array([1.0, 2.0, 3.0, 4.0], dtype=cp.float32)
+
+# 获取数据的 ctypes 指针
+data_ptr = arr.data.ptr
+print("Numpy data pointer:", data_ptr)
+print("type:", type(data_ptr))
+# 获取指针的原始地址，并使用 c_void_p 包装
+void_ptr = ctypes.c_void_p(data_ptr)
+print("Wrapped void pointer:", void_ptr)
+print("type:", type(void_ptr))
+print("Numpy data pointer:", data_ptr)
+print("Wrapped void pointer:", void_ptr)
