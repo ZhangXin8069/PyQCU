4e93306cf474ee3de946c0c125c85e5b5bd18b2c
zhangxin <zhangxin8069@qq.com>
2024-12-26 07:56:27 +0000

    2024-12-26-07-56-27

--- __init__.py
+++
@@ -1,5 +0,0 @@
-try:
-    import pyqcu.pyqcu as qcu
-except ImportError as e:
-    print(e)
-from .mpi import init
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from os import path
+from setuptools import setup, find_packages
 import subprocess
 from distutils.core import Extension, setup
 from setuptools.command.build_ext import build_ext
@@ -8,24 +8,20 @@ class CMakeBuild(build_ext):
     def run(self):
         subprocess.check_call(['bash','./setup.sh'])
         super().run()
-qcu_home = path.abspath("./qcu")
-VERSION = "0.0.1"
-LICENSE = "MIT"
-DESCRIPTION = "Python wrapper for qcu written in Cython."
 extensions = [
     Extension(
         "pyqcu.pointer",
-        ["./pointer.pyx"],
-        include_dirs=[numpy.get_include()],
+        ["./src/python/pointer.pyx"],
+        include_dirs=["./include/python", numpy.get_include()],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         language="c",
     ),
     Extension(
-            "pyqcu.pyqcu",
-            ["./pyqcu.pyx"],
-            include_dirs=["./include", numpy.get_include()],
+            "pyqcu.qcu",
+            ["./src/python/pyqcu.pyx"],
+            include_dirs=["./include/cpp", "./include/python", numpy.get_include()],
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-            library_dirs=[qcu_home],
+            library_dirs=["./lib"],
             libraries=["qcu"],
             language="c",
         )
@@ -34,21 +30,19 @@ ext_modules = cythonize(
     extensions,
     language_level="3",
 )
-packages = [
-    "pyqcu",
-]
-package_dir = {
-    "pyqcu": ".",
-}
+
 setup(
     name="pyqcu",
-    version=VERSION,
-    description=DESCRIPTION,
+    version="0.0.1",
+    description="Python wrapper for qcu written in Cython.",
     author="ZhangXin8069",
     author_email="zhangxin8069@qq.com",
-    packages=packages,
+    packages=find_packages(exclude=["test", "test.*"]),
     ext_modules=ext_modules,
-    license=LICENSE,
-    package_dir=package_dir,
+    license="MIT",
     cmdclass={'build_ext': CMakeBuild},
+    python_requires=">=3.6",
+    install_requires=["numpy", "scipy", "mpi4py", "cython", "cupy"],
+    test_suite="test"
+    
 )
