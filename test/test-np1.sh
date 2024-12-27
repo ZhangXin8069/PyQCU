e52fb5b2677892488d3dac76c415758d5aca3579
zhangxin <zhangxin8069@qq.com>
2024-12-27 09:48:15 +0000

    2024-12-27-09-48-15

--- test/test-np1.sh
+++ test/test-np1.sh
@@ -1,4 +1,6 @@
 pushd ../
+source ./env.sh
 bash ./install.sh
 popd
-mpirun --allow-run-as-root -n 1 python ./test.pyqcu.py
+mpirun --allow-run-as-root -n 1 python ./test.wilson.bistacg.py 
+# mpirun --allow-run-as-root -n 1 python ./test.wilson.dslash.py 
