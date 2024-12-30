e138617d81d458272927e176d46d27873b5d610c
zhangxin <zhangxin8069@qq.com>
2024-12-30 09:24:54 +0000

    2024-12-30-09-24-54

--- test/test.wilson.bistabcg.dslash.eigen.py
+++ test/test.wilson.bistabcg.dslash.eigen.py
@@ -1,84 +1,58 @@
 from pyqcu import qcu
 import cupy as cp
-from cupyx.scipy.sparse import linalg as clinalg
-from scipy.sparse import linalg as slinalg
 import numpy as np
 import re
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 print('My rank is ', rank)
-gauge_filename = "quda_wilson-bistabcg-gauge_-32-32-32-32-1048576-1-1-1-1-0-0-1-0-f.bin"
+gauge_filename = "quda_wilson-dslash-gauge_-32-32-32-32-1048576-1-1-1-1-0-0-1-0-f.bin"
 pattern = r"-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-"
 match = re.search(pattern, gauge_filename)
 if match:
     param = [int(num) for num in match.groups()]
     print("Extracted integers:", param)
     param.append(1000)
-    param.append(0)
+    param.append(5)
+    param.append(1)
     params = np.array(param, dtype=np.int32)
     print("NumPy Array:", params)
     print("Numpy data pointer:", params.data)
-    argv = np.array([0.0, 1e-9], dtype=np.float32)
+    argv = np.array([0, 1e-9], dtype=np.float32)
     print("Argv:", argv)
     print("Argv data pointer:", argv.data)
+    set_ptrs = np.array(params, dtype=np.int64)
+    print("Set ptrs:", set_ptrs)
+    print("Set ptrs data pointer:", set_ptrs.data)
+    qcu.applyInitQcu(set_ptrs, params, argv)
     _LAT_XYZT_ = 4
     _LAT_DCC_ = 36
-    _LAT_SC_ = 12
     _LAT_HALF_SC_ = 6
     size = params[_LAT_XYZT_]*_LAT_DCC_
     gauge_filename = gauge_filename.replace("gauge", "gauge")
     gauge = cp.fromfile(gauge_filename, dtype=cp.complex64, count=size)
     print("Gauge:", gauge)
     print("Gauge data:", gauge.data)
-    # size = params[_LAT_XYZT_]*_LAT_SC_
-    # fermion_in_filename = gauge_filename.replace("gauge", "fermion-in")
-    # fermion_in = cp.fromfile(
-    #     fermion_in_filename, dtype=cp.complex64, count=size)
-    # print("Fermion in:", fermion_in)
-    # print("Fermion in data:", fermion_in.data)
-    # fermion_out_filename = gauge_filename.replace("gauge", "fermion-out")
-    # quda_fermion_out = cp.fromfile(
-    #     fermion_out_filename, dtype=cp.complex64, count=size)
-    # fermion_out = cp.zeros(size, dtype=cp.complex64)
-    # print("Fermion out:", fermion_out)
-    # print("Fermion out data:", fermion_out.data)
-    # qcu.applyWilsonBistabCgDslashQcu(
-    #     fermion_out, fermion_in, gauge, params, argv)
-    # fermion_out_filename = fermion_out_filename.replace("quda", "pyqcu")
-    # fermion_out.tofile(fermion_out_filename)
-    # print("Fermion out diff:", cp.linalg.norm(fermion_out -
-    #       quda_fermion_out)/cp.linalg.norm(quda_fermion_out))
     size = params[_LAT_XYZT_]*_LAT_HALF_SC_
-    ##############################CUPY################################
-    def cwilson_bistabcg_dslash(fermion_in):
-        print("fermion_in norm:", cp.linalg.norm(fermion_in))
-        fermion_out = cp.zeros(size, dtype=cp.complex64)
-        # qcu.applyWilsonDslashQcu(
-        #     fermion_out, fermion_in, gauge, params, argv)
-        qcu.applyWilsonBistabCgDslashQcu(
-            fermion_out, fermion_in, gauge, params, argv)
-        print("fermion_out norm:", cp.linalg.norm(fermion_out))
-        return fermion_out
-    A = clinalg.LinearOperator(
-        shape=(size, size), matvec=cwilson_bistabcg_dslash)
-    evals, evecs = clinalg.eigsh(a=A, k=80, which="SA", tol=1e-1)
-    print("Eigenvalues:", evals)
-    print("Eigenvectors:", evecs)
-    # ###############################SCIPY################################
-    # def swilson_bistabcg_dslash(fermion_in):
-    #     print("fermion_in norm:", np.linalg.norm(fermion_in))
-    #     _fermion_in = cp.asarray(fermion_in)
-    #     _fermion_out = cp.zeros(size, dtype=cp.complex64)
-    #     # qcu.applyWilsonDslashQcu(
-    #     #     fermion_out, fermion_in, gauge, params, argv)
-    #     qcu.applyWilsonBistabCgDslashQcu(
-    #         _fermion_out, _fermion_in, gauge, params, argv)
-    #     fermion_out = _fermion_out.get()
-    #     print("fermion_out norm:", np.linalg.norm(fermion_out))
-    #     return fermion_out
-    # A = slinalg.LinearOperator(
-    #     shape=(size, size), matvec=swilson_bistabcg_dslash)
-    # evals, evecs = slinalg.eigsh(A, k=24, which="SA",tol=1e-1)
+    fermion_in_filename = gauge_filename.replace("gauge", "fermion-in")
+    fermion_in = cp.fromfile(
+        fermion_in_filename, dtype=cp.complex64, count=size)
+    print("Fermion in:", fermion_in)
+    print("Fermion in data:", fermion_in.data)
+    fermion_out_filename = gauge_filename.replace("gauge", "fermion-out")
+    quda_fermion_out = cp.fromfile(
+        fermion_out_filename, dtype=cp.complex64, count=size)
+    fermion_out = cp.zeros(size, dtype=cp.complex64)
+    print("Fermion out:", fermion_out)
+    print("Fermion out data:", fermion_out.data)
+    qcu.applyWilsonBistacbDslashQcu(fermion_out, fermion_in, gauge, set_ptrs, params)
+    fermion_out_filename = fermion_out_filename.replace("quda", "pyqcu")
+    fermion_out.tofile(fermion_out_filename)
+    print("Fermion out diff:", cp.linalg.norm(fermion_out -
+          quda_fermion_out)/cp.linalg.norm(quda_fermion_out))
+    qcu.applyEndQcu(set_ptrs, params)
+    print("params:", params)
+    print("argv:", argv)
+    print("set_ptrs:", set_ptrs)
 else:
     print("No match found!")
