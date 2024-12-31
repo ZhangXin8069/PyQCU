fdc8a4aa1ffaf66a58583f2a58f8bf764a9e9c5f
zhangxin <zhangxin8069@qq.com>
2024-12-31 05:46:23 +0000

    2024-12-31-05-46-23

--- pyqcu/__init__.py
+++ pyqcu/__init__.py
@@ -3,6 +3,7 @@ print(
     @@@@@@######QCU NOTES START######@@@@@@@
     1. The libqcu.so was compiled when pyqcu setup in download_path/PyQCU/lib, please add this path to your LD_LIBRARY_PATH.
     2. The QCU splite grid by x->y->z->t, lattice by x->y->z->t->p->d->c->c or x->y->z->t->c->s(->p) and x->y->z->t->c->s->c->s(->p).
-    3. The QCU input params in numpy array(dtype=np.int32), argv in  numpy array(dtype=np.float32 or float64) array, set_ptrs in numpy array(dtype=np.int64), other in cupy array(dtype=cp.complex64 or complex128).
+    3. The QUDA(PyQUDA) splite grid by t->z->y->x, lattice by c->c->x->y->z->t->p->d or c->s->x->y->z->t(->p) and c->s->c->s->x->y->z->t(->p).
+    4. The QCU input params in numpy array(dtype=np.int32), argv in  numpy array(dtype=np.float32 or float64) array, set_ptrs in numpy array(dtype=np.int64), other in cupy array(dtype=cp.complex64 or complex128).
      @@@@@@######QCU NOTES END######@@@@@@@
     ''')
