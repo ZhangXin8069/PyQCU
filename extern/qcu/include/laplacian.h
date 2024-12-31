16549e9aa51e6a81c98c0a807a76a4e44d55928f
zhangxin <zhangxin8069@qq.com>
2024-12-31 13:18:35 +0000

    2024-12-31-13-18-35

--- extern/qcu/include/laplacian.h
+++ extern/qcu/include/laplacian.h
@@ -4,36 +4,36 @@ namespace qcu
 {
   template <typename T>
   __global__ void laplacian_inside(void *device_U, void *device_src,
-                                       void *device_dest, void *device_params);
+                                   void *device_dest, void *device_params);
   template <typename T>
   __global__ void laplacian_x_send(void *device_U, void *device_src,
-                                       void *device_params,
-                                       void *device_b_x_send_vec,
-                                       void *device_f_x_send_vec);
+                                   void *device_params,
+                                   void *device_b_x_send_vec,
+                                   void *device_f_x_send_vec);
   template <typename T>
-  __global__ void laplacian_x_recv(void *device_U, void *device_dest,
-                                       void *device_params,
-                                       void *device_b_x_recv_vec,
-                                       void *device_f_x_recv_vec);
+  __global__ void laplacian_x_recv(void *device_dest,
+                                   void *device_params,
+                                   void *device_b_x_recv_vec,
+                                   void *device_f_x_recv_vec);
   template <typename T>
   __global__ void laplacian_y_send(void *device_U, void *device_src,
-                                       void *device_params,
-                                       void *device_b_y_send_vec,
-                                       void *device_f_y_send_vec);
+                                   void *device_params,
+                                   void *device_b_y_send_vec,
+                                   void *device_f_y_send_vec);
   template <typename T>
-  __global__ void laplacian_y_recv(void *device_U, void *device_dest,
-                                       void *device_params,
-                                       void *device_b_y_recv_vec,
-                                       void *device_f_y_recv_vec);
+  __global__ void laplacian_y_recv(void *device_dest,
+                                   void *device_params,
+                                   void *device_b_y_recv_vec,
+                                   void *device_f_y_recv_vec);
   template <typename T>
   __global__ void laplacian_z_send(void *device_U, void *device_src,
-                                       void *device_params,
-                                       void *device_b_z_send_vec,
-                                       void *device_f_z_send_vec);
+                                   void *device_params,
+                                   void *device_b_z_send_vec,
+                                   void *device_f_z_send_vec);
   template <typename T>
-  __global__ void laplacian_z_recv(void *device_U, void *device_dest,
-                                       void *device_params,
-                                       void *device_b_z_recv_vec,
-                                       void *device_f_z_recv_vec);
+  __global__ void laplacian_z_recv(void *device_dest,
+                                   void *device_params,
+                                   void *device_b_z_recv_vec,
+                                   void *device_f_z_recv_vec);
 }
 #endif
