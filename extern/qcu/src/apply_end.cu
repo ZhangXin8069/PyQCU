8e0ef2f19ea6799fad780ac884b3affc6827dfe0
zhangxin <zhangxin8069@qq.com>
2025-01-01 13:32:19 +0000

    2025-01-01-13-32-19

--- extern/qcu/src/apply_end.cu
+++ extern/qcu/src/apply_end.cu
@@ -3,27 +3,24 @@
 #pragma optimize(5)
 using namespace qcu;
 using T = float;
-void applyInitQcu(long long _set_ptrs, long long _params, long long _argv)
+void applyEndQcu(long long _set_ptrs, long long _params)
 {
     void *set_ptrs = (void *)_set_ptrs;
-    void *argv = (void *)_argv;
     void *params = (void *)_params;
     int set_index = static_cast<int *>(params)[_SET_INDEX_];
-    // init for lattice_set
-    LatticeSet<T> *set_ptr = new LatticeSet<T>();
+    // end for lattice_set
+    LatticeSet<T> *set_ptr = static_cast<LatticeSet<T> *>((void *)(static_cast<long long *>(set_ptrs)[set_index]));
     auto start = std::chrono::high_resolution_clock::now();
-    set_ptr->give(params, argv);
-    set_ptr->init();
     set_ptr->_print();
+    set_ptr->end();
     printf("set_ptr:%p\n", set_ptr);
     printf("long long set_ptr:%lld\n", (long long)set_ptr);
-    static_cast<long long *>(set_ptrs)[set_index] = (long long)set_ptr;
     auto end = std::chrono::high_resolution_clock::now();
     auto duration =
         std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     cudaError_t err = cudaGetLastError();
     checkCudaErrors(err);
-    printf("lattice set init total time:%.9lf "
+    printf("lattice set end total time:%.9lf "
            "sec\n",
            double(duration) / 1e9);
 }
