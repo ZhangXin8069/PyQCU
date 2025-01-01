2491252f05841b5d43d0852b8642f0512e004f4d
zhangxin <zhangxin8069@qq.com>
2025-01-01 06:37:52 +0000

    2025-01-01-06-37-52

--- test/test-npX.sh
+++ test/test-npX.sh
@@ -2,12 +2,12 @@
 command=$@
 pushd ../
 source ./env.sh
-bash ./install.sh
+# bash ./install.sh
 popd
 # mpirun --allow-run-as-root -n ${command} python ./test.io.py
 # mpirun --allow-run-as-root -n ${command} python ./test.set.py
 # mpirun --allow-run-as-root -n ${command} python ./test.wilson.cg.py
-mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistacg.py
-# mpirun --allow-run-as-root -n ${command} python ./test.wilson.dslash.py
+# mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistacg.py
+mpirun --allow-run-as-root -n ${command} python ./test.wilson.dslash.py
 # mpirun --allow-run-as-root -n ${command} python ./test.clover.dslash.py
 # mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistabcg.dslash.eigen.py
