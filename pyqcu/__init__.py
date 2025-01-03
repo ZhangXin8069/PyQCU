7a2023080bbdec3f90ca8cf79789a0e5ce7fb8e2
zhangxin <zhangxin8069@qq.com>
2025-01-03 06:22:57 +0000

    2025-01-03-06-22-57

--- pyqcu/__init__.py
+++ pyqcu/__init__.py
@@ -5,5 +5,6 @@ print(
     2. The QCU(PyQCU) splite grid by x->y->z->t, lattice by x->y->z->t->p->d->c->c or x->y->z->t->c->s(->p) and x->y->z->t->c->s->c->s(->p).
     3. The QUDA(PyQUDA) splite grid by t->z->y->x, lattice by c->c->x->y->z->t->p->d or c->s->x->y->z->t(->p) and c->s->c->s->x->y->z->t(->p).
     4. The QCU input params in numpy array(dtype=np.int32), argv in  numpy array(dtype=np.float32 or float64) array, set_ptrs in numpy array(dtype=np.int64), other in cupy array(dtype=cp.complex64 or complex128).
-     @@@@@@######QCU NOTES END######@@@@@@@
+    5. The smallest lattice size is (x=4,y=4,z=4,t=8) that QCU support.
+    @@@@@@######QCU NOTES END######@@@@@@@
     ''')
