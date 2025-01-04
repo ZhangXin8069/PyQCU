f1d43090467c609661380da3874fbc5bb8509b0a
zhangxin <zhangxin8069@qq.com>
2025-01-04 02:36:59 +0000

    2025-01-04-02-36-59

--- test/test-npX.sh
+++ test/test-npX.sh
@@ -6,9 +6,9 @@ source ./env.sh
 popd
 # mpirun --allow-run-as-root -n ${command} python ./test.io.py
 # mpirun --allow-run-as-root -n ${command} python ./test.set.py
-# mpirun --allow-run-as-root -n ${command} python ./test.laplacian.py
+mpirun --allow-run-as-root -n ${command} python ./test.laplacian.py
 # mpirun --allow-run-as-root -n ${command} python ./test.wilson.cg.py
 # mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistacg.py
 # mpirun --allow-run-as-root -n ${command} python ./test.wilson.dslash.py
 # mpirun --allow-run-as-root -n ${command} python ./test.clover.dslash.py
-mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistabcg.dslash.eigen.py
+# mpirun --allow-run-as-root -n ${command} python ./test.wilson.bistabcg.dslash.eigen.py
