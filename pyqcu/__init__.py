9e520a9bab69d0d67fa655f87ad700596568e81c
zhangxin <zhangxin8069@qq.com>
2025-01-13 02:41:31 +0000

    2025-01-13-02-41-31

--- pyqcu/__init__.py
+++ pyqcu/__init__.py
@@ -1,7 +1,7 @@
 print(
     '''
     @@@@@@######QCU NOTES START######@@@@@@@
-    0. Required: MPI(eq-4.1.2), CUDA(eq-12.4), CMAKE(eq-3.22.1), GCC(eq-11.4.0), HDF5-MPI(eq-1.10.7,'apt install libhdf5-mpi-dev && export HDF5_MPI="ON" && pip install --no-binary=h5py h5py').
+    0. Required: MPI(e.g. 4.1.2), CUDA(e.g. 12.4), CMAKE(e.g. 3.22.1), GCC(e.g. 11.4.0), HDF5-MPI(e.g. 1.10.7,'apt install libhdf5-mpi-dev && export HDF5_MPI="ON" && pip install --no-binary=h5py h5py').
     1. The libqcu.so was compiled when pyqcu setup in download_path/PyQCU/lib, please add this path to your LD_LIBRARY_PATH.
     2. The QCU(PyQCU) splite grid by x->y->z->t, lattice by x->y->z->t->p->d->c->c or x->y->z->t->c->s(->p) and x->y->z->t->c->s->c->s(->p).
     3. The QUDA(PyQUDA) splite grid by t->z->y->x, lattice by c->c->x->y->z->t->p->d or c->s->x->y->z->t(->p) and c->s->c->s->x->y->z->t(->p).
