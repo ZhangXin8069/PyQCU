ba2554afd87fdf04e09a8174d792834ac4fe751f
zhangxin <zhangxin8069@qq.com>
2025-01-24 17:27:25 +0800

    2025-01-24-17-27-25

--- test/ssub-test5.sh
+++ test/ssub-test5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-#SBATCH --job-name=ssub
+#SBATCH --job-name=ssub-test5
 #SBATCH --partition=h20-nettr
 #SBATCH --nodes=1
 #SBATCH -n 8
 #SBATCH --time=00-00:30:00
-#SBATCH --output=ssub.out
-#SBATCH --error=ssub.err
+#SBATCH --output=ssub-test5.out
+#SBATCH --error=ssub-test5.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhangxin8069@qq.com
 #SBATCH --gres=gpu:8
