c4344bc1698a7f521f1385a420fec5e4d39eb4ef
zhangxin <zhangxin8069@qq.com>
2025-02-24 08:30:29 +0000

    2025-02-24-08-30-29

--- pyqcu/linalg.py
+++ pyqcu/linalg.py
@@ -2,10 +2,10 @@ import cupy as cp
 
 def gram_schmidt(vectors):
     num_vectors = vectors.shape[1]
-    orthogonal_vectors = cp.copy(vectors)
+    orthogonal_vectors = cp.copy(vectors).astype(cp.complex128)
     for i in range(num_vectors):
         orthogonal_vectors[:, i] /= cp.linalg.norm(orthogonal_vectors[:, i])
         for j in range(i + 1, num_vectors):
-            projection = cp.dot(orthogonal_vectors[:, j], orthogonal_vectors[:, i])
+            projection = cp.dot(orthogonal_vectors[:, j].conj(), orthogonal_vectors[:, i])
             orthogonal_vectors[:, j] -= projection * orthogonal_vectors[:, i]
     return orthogonal_vectors
