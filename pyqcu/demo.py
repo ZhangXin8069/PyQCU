dc28e127f9457cf7813e6793154c5326f055060b
zhangxin <zhangxin8069@qq.com>
2025-04-11 18:25:04 +0000

    2025-04-11-18-25-04

--- pyqcu/demo.py
+++ pyqcu/demo.py
@@ -1,6 +1,8 @@
 import cupy as cp
 import numpy as np
 from pyqcu import qcu, define, gauge, linalg, io
+
+
 def give(params, argv=None, set_ptrs=None, sigma=0.1, seed=12138):
     if argv is None:
         from pyqcu.set import argv
@@ -8,7 +10,24 @@ def give(params, argv=None, set_ptrs=None, sigma=0.1, seed=12138):
         from pyqcu.set import set_ptrs
     print('My rank is ', define.rank)
     print("Parameters:", params)
+    src = cp.ones(params[define._LAT_XYZT_]*define._LAT_SC_,
+                  dtype=define.dtype(params[define._DATA_TYPE_]))
+    src = io.fermion2psctzyx(src, params)
+    print("Src data:", src.data)
+    print("Src shape:", src.shape)
+    argv = argv.astype(src.real.dtype)
     print("Arguments:", argv)
+    print("Arguments data:", argv.data)
+    print("Arguments dtype:", argv.dtype)
+    print("Demo is running...")
+    print("Set pointers:", set_ptrs)
+    print("Set pointers data:", set_ptrs.data)
+    U = gauge.give_gauss_SU3(sigma=sigma, seed=seed,
+                             dtype=src.dtype, size=params[define._LAT_XYZT_]*define._LAT_D_)
+    U = io.gauge2dptzyxcc(U, params)
+    U = io.dptzyxcc2ccdptzyx(U)
+    print("U data:", U.data)
+    print("U shape:", U.shape)
     wilson_cg_params = params.copy()
     wilson_cg_params[define._SET_INDEX_] = 0
     wilson_cg_params[define._SET_PLAN_] = define._SET_PLAN1_
@@ -37,33 +56,20 @@ def give(params, argv=None, set_ptrs=None, sigma=0.1, seed=12138):
     wilson_dslash_oe_dag_params[define._PARITY_] = define._ODD_
     wilson_dslash_oe_dag_params[define._DAGGER_] = define._USE_
     qcu.applyInitQcu(set_ptrs, wilson_dslash_oe_dag_params, argv)
-    print("Demo is running...")
-    print("Set pointers:", set_ptrs)
-    print("Set pointers data:", set_ptrs.data)
-    src = cp.zeros(int(params[define._LAT_XYZT_]*define._LAT_SC_ /
-                   define._LAT_P_), dtype=define.dtype(params[define._DATA_TYPE_]))
-    src = linalg.initialize_random_vector(src)
-    src = io.fermion2sctzyx(src, params)
-    print("Src data:", src.data)
-    print("Src shape:", src.shape)
     dest = cp.zeros_like(src)
-    U = gauge.give_gauss_SU3(sigma=sigma, seed=seed,
-                             dtype=define.dtype(params[define._DATA_TYPE_]), size=params[define._LAT_XYZT_]*define._LAT_D_)
-    U = io.gauge2dptzyxcc(U, params)
-    U = io.dptzyxcc2ccdptzyx(U)
-    print("U data:", U.data)
-    print("U shape:", U.shape)
     qcu.applyWilsonBistabCgQcu(dest, src,
                                U, set_ptrs, wilson_cg_params)
     print("Dest data:", dest.data)
     print("Dest shape:", dest.shape)
-    return dest, src, U, set_ptrs, wilson_cg_params, wilson_dslash_eo_params, wilson_dslash_oe_params, wilson_dslash_eo_dag_params, wilson_dslash_oe_dag_params
-def end(set_ptrs):
+    return U, src, dest, set_ptrs, wilson_cg_params, wilson_dslash_eo_params, wilson_dslash_oe_params, wilson_dslash_eo_dag_params, wilson_dslash_oe_dag_params
+
+
+def end(set_ptrs, params):
     print("Set pointers:", set_ptrs)
     print("Set pointers data:", set_ptrs.data)
-    params = np.zeros(define._PARAMS_SIZE_, dtype=np.int32)
+    _params = params.copy()
     for i in range(len(set_ptrs)):
         if set_ptrs[i] != 0:
-            params[define._SET_INDEX_] = i
+            _params[define._SET_INDEX_] = i
             print(f"Set pointers[{i}] data: {set_ptrs[i]} has been freed")
-            qcu.applyEndQcu(set_ptrs, params)
+            qcu.applyEndQcu(set_ptrs, _params)
