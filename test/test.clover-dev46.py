020dff3a69c1705115fdcc778458ab14c3e6d83d
zhangxin <zhangxin8069@qq.com>
2025-04-17 07:00:34 +0000

    2025-04-17-07-00-34

---
+++ test/test.clover-dev46.py
@@ -0,0 +1,49 @@
+import re
+import numpy as np
+import cupy as cp
+from pyqcu import define
+from pyqcu import io
+from pyqcu import qcu
+from pyqcu.set import params, argv, set_ptrs
+import h5py
+print('My rank is ', define.rank)
+params[define._LAT_Y_] /= 2
+params[define._LAT_XYZT_] /= 2
+params[define._SET_PLAN_] = 2
+gauge_filename = f"quda_wilson-clover-dslash-gauge_-{params[define._LAT_X_]}-{params[define._LAT_Y_]}-{params  [define._LAT_Z_]}-{params[define._LAT_T_]}-{params[define._LAT_XYZT_]}-{params[define._GRID_X_]}-{params[define._GRID_Y_]}-{params[define._GRID_Z_]}-{params[define._GRID_T_]}-{params[define._PARITY_]}-{params[define._NODE_RANK_]}-{params[define._NODE_SIZE_]}-{params[define._DAGGER_]}-f.h5"
+params[define._GRID_T_] = 1
+params[define._NODE_RANK_] = define.rank
+params[define._NODE_SIZE_] = define.size
+params[define._DATA_TYPE_] = define._LAT_C64_
+print("Parameters:", params)
+#############################
+print("Gauge filename:", gauge_filename)
+gauge = io.hdf5_xxxtzyx2grid_xxxtzyx(params, gauge_filename)
+fermion_in_filename = gauge_filename.replace("gauge", "fermion-in")
+print("Fermion in filename:", fermion_in_filename)
+fermion_in = io.hdf5_xxxtzyx2grid_xxxtzyx(params, fermion_in_filename)
+fermion_out_filename = gauge_filename.replace("gauge", "fermion-out")
+print("Fermion out filename:", fermion_out_filename)
+quda_fermion_out = io.hdf5_xxxtzyx2grid_xxxtzyx(params, fermion_out_filename)
+#############################
+fermion_out = cp.zeros_like(fermion_in)
+print("Fermion out data:", fermion_out.data)
+print("Fermion out shape:", fermion_out.shape)
+#############################
+qcu.applyInitQcu(set_ptrs, params, argv)
+clover_even = cp.zeros((define._LAT_S_, define._LAT_C_, define._LAT_S_, define._LAT_C_,
+                       params[define._LAT_T_], params[define._LAT_Z_], params[define._LAT_Y_], int(params[define._LAT_X_]/define._LAT_P_),), dtype=fermion_in.dtype)
+qcu.applyCloverQcu(clover_even, gauge, set_ptrs, params)
+qcu.applyDslashQcu(fermion_out, fermion_in,
+                   clover_even, gauge, set_ptrs, params)
+# qcu.applyCloverDslashQcu(fermion_out, fermion_in, gauge, set_ptrs, params)
+qcu.applyEndQcu(set_ptrs, params)
+#############################
+print("Fermion out data:", fermion_out.data)
+print("Fermion out shape:", fermion_out.shape)
+print("QUDA Fermion out data:", quda_fermion_out.data)
+print("QUDA Fermion out shape:", quda_fermion_out.shape)
+print("Difference:", cp.linalg.norm(fermion_out -
+      quda_fermion_out)/cp.linalg.norm(quda_fermion_out))
+#############################
+io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_out, params)
