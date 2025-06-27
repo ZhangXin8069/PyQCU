8503ca5266afe832f6a69cc6e033b73c25c17ee3
zhangxin <zhangxin8069@qq.com>
2025-06-27 06:14:47 +0000

    2025-06-27-06-14-47

--- extern/qcu/include/define.h
+++ extern/qcu/include/define.h
@@ -3,7 +3,7 @@
 #include "./lattice_complex.h"
 namespace qcu
 {
-#define _BLOCK_SIZE_ 32 // for test small lattice
+#define _BLOCK_SIZE_ 16 // for test small lattice
 // #define _BLOCK_SIZE_ 128
 #define _MAIN_RANK_ 0
 #define _a_ 0
@@ -301,16 +301,35 @@ namespace qcu
       U[i] = zero;              \
     }                           \
   }
+// #define give_u(U, tmp_U, lat_tzyx)                       \
+//   {                                                      \
+//     for (int i = 0; i < _LAT_2C_; i++)                   \
+//     {                                                    \
+//       U[i] = tmp_U[i * _LAT_D_ * _EVEN_ODD_ * lat_tzyx]; \
+//     }                                                    \
+//     U[6] = (U[1] * U[5] - U[2] * U[4]).conj();           \
+//     U[7] = (U[2] * U[3] - U[0] * U[5]).conj();           \
+//     U[8] = (U[0] * U[4] - U[1] * U[3]).conj();           \
+  }
 #define give_u(U, tmp_U, lat_tzyx)                       \
   {                                                      \
-    for (int i = 0; i < _LAT_2C_; i++)                   \
+    for (int i = 0; i < _LAT_CC_; i++)                   \
     {                                                    \
       U[i] = tmp_U[i * _LAT_D_ * _EVEN_ODD_ * lat_tzyx]; \
     }                                                    \
-    U[6] = (U[1] * U[5] - U[2] * U[4]).conj();           \
-    U[7] = (U[2] * U[3] - U[0] * U[5]).conj();           \
-    U[8] = (U[0] * U[4] - U[1] * U[3]).conj();           \
   }
+// #define give_u(U, tmp_U, lat_tzyx) \
+//   {                                \
+//     U[0]._data.x = 1.0;            \
+//     U[1]._data.x = 0.0;            \
+//     U[2]._data.x = 0.0;            \
+//     U[3]._data.x = 0.0;            \
+//     U[4]._data.x = 1.0;            \
+//     U[5]._data.x = 0.0;            \
+//     U[6]._data.x = 0.0;            \
+//     U[7]._data.x = 0.0;            \
+//     U[8]._data.x = 1.0;            \
+//   }
 #define _give_u_comm(parity, U, tmp_U, _lat_tzyx)                    \
   {                                                                  \
     for (int i = 0; i < _LAT_2C_; i++)                               \
