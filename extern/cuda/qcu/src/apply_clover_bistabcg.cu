f07b59c5ed115e04643f030df746585ed328d6c7
zhangxin <zhangxin8069@qq.com>
2025-08-27 07:33:19 +0000

    2025-08-27-07-33-19

--- extern/cuda/qcu/src/apply_clover_bistabcg.cu
+++ extern/cuda/qcu/src/apply_clover_bistabcg.cu
@@ -2,13 +2,16 @@
 #include "../include/qcu.h"
 #pragma optimize(5)
 using namespace qcu;
-void applyCloverBistabCgQcu(long long _fermion_out, long long _fermion_in, long long _gauge, long long _clover, long long _set_ptrs, long long _params)
+void applyCloverBistabCgQcu(long long _fermion_out, long long _fermion_in, long long _gauge, long long _clover_ee, long long _clover_oo, long long _clover_ee_inv, long long _clover_oo_inv, long long _set_ptrs, long long _params)
 {
   cudaDeviceSynchronize();
   void *fermion_out = (void *)_fermion_out;
   void *fermion_in = (void *)_fermion_in;
   void *gauge = (void *)_gauge;
-  void *clover = (void *)_clover;
+  void *clover_ee = (void *)_clover_ee;
+  void *clover_oo = (void *)_clover_oo;
+  void *clover_ee_inv = (void *)_clover_ee_inv;
+  void *clover_oo_inv = (void *)_clover_oo_inv;
   void *set_ptrs = (void *)_set_ptrs;
   void *params = (void *)_params;
   int set_index = static_cast<int *>(params)[_SET_INDEX_];
@@ -21,7 +24,7 @@ void applyCloverBistabCgQcu(long long _fermion_out, long long _fermion_in, long 
     // ptzyxsc2psctzyx<float>(fermion_out, &_set);
     LatticeCloverBistabCg<float> _bistabcg;
     _bistabcg.give(set_ptr);
-    _bistabcg.init(fermion_out, fermion_in, gauge, clover);
+    _bistabcg.init(fermion_out, fermion_in, gauge, clover_ee, clover_oo, clover_ee_inv, clover_oo_inv);
     if (set_ptr->host_params[_VERBOSE_])
     {
       _bistabcg.run_test();
@@ -43,7 +46,7 @@ void applyCloverBistabCgQcu(long long _fermion_out, long long _fermion_in, long 
     // ptzyxsc2psctzyx<double>(fermion_out, &_set);
     LatticeCloverBistabCg<double> _bistabcg;
     _bistabcg.give(set_ptr);
-    _bistabcg.init(fermion_out, fermion_in, gauge, clover);
+    _bistabcg.init(fermion_out, fermion_in, gauge, clover_ee, clover_oo, clover_ee_inv, clover_oo_inv);
     if (set_ptr->host_params[_VERBOSE_])
     {
       _bistabcg.run_test();
--- pyqcu/cuda/qcu/qcu.pyx
+++ pyqcu/cuda/qcu/qcu.pyx
@@ -6,6 +6,11 @@ cdef long long set_ptrs
 cdef long long fermion_out
 cdef long long fermion_in
 cdef long long clover
+cdef long long clover_inv
+cdef long long clover_ee
+cdef long long clover_oo
+cdef long long clover_ee_inv
+cdef long long clover_oo_inv
 cdef long long gauge
 cdef long long params
 cdef long long argv
@@ -87,6 +92,13 @@ def applyCloverQcu(_clover, _gauge, _set_ptrs, _params):
     set_ptrs = _set_ptrs.ctypes.data
     params = _params.ctypes.data
     qcu.applyCloverQcu(clover, gauge, set_ptrs, params)
+def applyCloversQcu(_clover, _clover_inv, _gauge, _set_ptrs, _params):
+    clover = _clover.data.ptr
+    clover_inv = _clover_inv.data.ptr
+    gauge = _gauge.data.ptr
+    set_ptrs = _set_ptrs.ctypes.data
+    params = _params.ctypes.data
+    qcu.applyCloversQcu(clover, clover_inv, gauge, set_ptrs, params)
 def applyDslashQcu(_fermion_out, _fermion_in, _gauge, _clover, _set_ptrs, _params):
     fermion_out = _fermion_out.data.ptr
     fermion_in = _fermion_in.data.ptr
@@ -100,11 +112,14 @@ def applyGaussGaugeQcu(_gauge, _set_ptrs, _params):
     set_ptrs = _set_ptrs.ctypes.data
     params = _params.ctypes.data
     qcu.applyGaussGaugeQcu(gauge, set_ptrs, params)
-def applyCloverBistabCgQcu(_fermion_out, _fermion_in, _gauge, _clover, _set_ptrs, _params):
+def applyCloverBistabCgQcu(_fermion_out, _fermion_in, _gauge, long long _clover_ee, long long _clover_oo, long long _clover_ee_inv, long long _clover_oo_inv, _set_ptrs, _params):
     fermion_out = _fermion_out.data.ptr
     fermion_in = _fermion_in.data.ptr
-    clover = _clover.data.ptr
+    clover_ee = _clover_ee.data.ptr
+    clover_oo = _clover_oo.data.ptr
+    clover_ee_inv = _clover_ee_inv.data.ptr
+    clover_oo_inv = _clover_oo_inv.data.ptr
     gauge = _gauge.data.ptr
     set_ptrs = _set_ptrs.ctypes.data
     params = _params.ctypes.data
-    qcu.applyCloverBistabCgQcu(fermion_out, fermion_in, gauge, clover, set_ptrs, params)
+    qcu.applyCloverBistabCgQcu(fermion_out, fermion_in, gauge, clover_ee, clover_oo, clover_ee_inv, clover_oo_inv, set_ptrs, params)
