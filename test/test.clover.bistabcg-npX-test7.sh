b78ec45ef113b5ce58d5c753fd898212f51cdbeb
zhangxin <zhangxin8069@qq.com>
2025-08-28 21:07:48 +0800

    2025-08-28-21-07-48

--- test/test.clover.bistabcg-npX-test7.sh
+++ test/test.clover.bistabcg-npX-test7.sh
@@ -7,4 +7,5 @@ popd
 mpirun -x UCX_TLS=self,sm,rc -np 4 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np4-test6.log 2>&1
 mpirun -x UCX_TLS=self,sm,rc -np 8 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np8-test6.log 2>&1
 mpirun -x UCX_TLS=self,sm,rc -np 16 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np16-test6.log 2>&1
-# mpirun -x UCX_TLS=self,sm,rc -np 32 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np32-test6.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 32 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np32-test6.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 64 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np64-test6.log 2>&1
