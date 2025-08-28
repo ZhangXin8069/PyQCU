6b2799752299913299e83e488e7166eaddd3ceff
zhangxin <zhangxin8069@qq.com>
2025-08-28 02:51:46 +0000

    2025-08-28-02-51-46

--- pyqcu/cuda/set.py
+++ pyqcu/cuda/set.py
@@ -24,14 +24,8 @@ params[define._MG_T_] = 8
 params[define._LAT_E_] = 24
 params[define._VERBOSE_] = 1
 params[define._SEED_] = 42
-print("Parameters:", params)
-print("Parameters data:", params.data)
 argv = np.array([0.0]*define._ARGV_SIZE_, dtype=np.float32)
 argv[define._MASS_] = -3.5  # make kappa=1.0
 argv[define._TOL_] = 1e-9
 argv[define._SIGMA_] = 0.1
-print("Arguments:", argv)
-print("Arguments data:", argv.data)
 set_ptrs = np.array(10*[0], dtype=np.int64)  # maybe more than 10?
-print("Set pointers:", set_ptrs)
-print("Set pointers data:", set_ptrs.data)
