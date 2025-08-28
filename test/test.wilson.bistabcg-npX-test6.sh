d0d0ad8e8d1acd21dfe2251a16904fd59ea949c9
zhangxin <zhangxin8069@qq.com>
2025-08-28 05:53:15 +0000

    2025-08-28-05-53-15

--- test/test.wilson.bistabcg-npX-test6.sh
+++ test/test.wilson.bistabcg-npX-test6.sh
@@ -1,6 +1,6 @@
-mpirun -np 1 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np1-test6.log 2>&1
-mpirun -np 2 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np2-test6.log 2>&1
-mpirun -np 4 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np4-test6.log 2>&1
-mpirun -np 8 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np8-test6.log 2>&1
-mpirun -np 16 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np16-test6.log 2>&1
-mpirun -np 32 python -u ./test.wilson.bistacg-test6.py >test.wilson.bistabcg-np32-test6.log 2>&1
+mpirun -np 1 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np1-test6.log 2>&1
+mpirun -np 2 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np2-test6.log 2>&1
+mpirun -np 4 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np4-test6.log 2>&1
+mpirun -np 8 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np8-test6.log 2>&1
+mpirun -np 16 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np16-test6.log 2>&1
+mpirun -np 32 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np32-test6.log 2>&1
