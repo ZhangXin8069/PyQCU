7adb62ae7cdf1d28c6f7db2ba497785290acd085
zhangxin <zhangxin8069@qq.com>
2025-08-28 07:54:17 +0000

    2025-08-28-07-54-17

--- pyqcu/cuda/__init__.py
+++ pyqcu/cuda/__init__.py
@@ -1,2 +1,12 @@
-import pyqcu.cuda.gauge as Gauge
-import pyqcu.cuda.io as Io
+try:
+    import pyqcu.cuda.gauge as Gauge
+except Exception as e:
+    print(f"Error: {e}")
+try:
+    import pyqcu.cuda.io as Io
+except Exception as e:
+    print(f"Error: {e}")
+try:
+    import pyqcu.cuda.qcu as Qcu
+except Exception as e:
+    print(f"Error: {e}")
