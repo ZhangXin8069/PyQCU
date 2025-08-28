126bac46a4530906835bf440e6069501afa5807f
zhangxin <zhangxin8069@qq.com>
2025-08-28 05:54:30 +0000

    2025-08-28-05-54-30

--- test/test.wilson.bistabcg-npX-test6.sh
+++ test/test.wilson.bistabcg-npX-test6.sh
@@ -1,3 +1,7 @@
+pushd ../
+source ./env.sh
+bash ./install.sh
+popd
 mpirun -np 1 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np1-test6.log 2>&1
 mpirun -np 2 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np2-test6.log 2>&1
 mpirun -np 4 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np4-test6.log 2>&1
