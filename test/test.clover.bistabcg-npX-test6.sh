4bcc512a1fd6be610239b27d3ffafd99dcf5430c
zhangxin <zhangxin8069@qq.com>
2025-08-28 09:45:16 +0000

    2025-08-28-09-45-16

--- test/test.clover.bistabcg-npX-test6.sh
+++ test/test.clover.bistabcg-npX-test6.sh
@@ -1,6 +1,6 @@
 pushd ../
-source ./env.sh
-bash ./install.sh
+# source ./env.sh
+# bash ./install.sh
 popd
 mpirun -np 1 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np1-test6.log 2>&1
 mpirun -np 2 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np2-test6.log 2>&1
--- test/test.wilson.bistabcg-npX-test6.sh
+++ test/test.wilson.bistabcg-npX-test6.sh
@@ -1,5 +1,5 @@
 pushd ../
-source ./env.sh
+# source ./env.sh
 #bash ./install.sh
 popd
 # mpirun -np 1 python -u ./test.wilson.bistabcg-test6.py >test.wilson.bistabcg-np1-test6.log 2>&1
