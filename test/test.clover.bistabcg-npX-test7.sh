899c8146feb033935f576a46867e100c7d9b7928
zhangxin <zhangxin8069@qq.com>
2025-08-29 19:45:35 +0800

    2025-08-29-19-45-35

--- test/test.clover.bistabcg-npX-test7.sh
+++ test/test.clover.bistabcg-npX-test7.sh
@@ -3,9 +3,9 @@ pushd ../
 # bash ./install.sh
 popd
 rm _*
-mpirun -x UCX_TLS=self,sm,rc -np 2 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np2-test7.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 4 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np4-test7.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 8 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np8-test7.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 16 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np16-test7.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 32 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np32-test7.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 64 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np64-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 2 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np2-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 4 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np4-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 8 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np8-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 16 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np16-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 32 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np32-test7.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 64 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.clover.bistabcg-test7.py > test.clover.bistabcg-np64-test7.log 2>&1
--- test/test.npX-test6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-command=$@
-pushd ../
-source ./env.sh
-popd
-mpirun -x UCX_TLS=self,sm,rc -n ${command} python -u ./test.wilson.bistacg-test6.py
