b74b68728d4899f28b528e96972f9fced3ea835b
zhangxin <zhangxin8069@qq.com>
2025-08-29 04:07:44 +0000

    2025-08-29-04-07-44

--- test/test.gauge-test8.py
+++ test/test.gauge-test8.py
@@ -4,10 +4,10 @@ from pyqcu.cuda import io
 from pyqcu.cuda import qcu
 from pyqcu.cuda.tmp_set import params, argv, set_ptrs
 #############################
-gauge = io.get_or_give(params, "gauge-test8.h5")
-clover = io.get_or_give(params, "clover-test8.h5")
-fermion_in = io.get_or_give(params, "fermion_in-test8.h5")
-fermion_out = io.get_or_give(params, "fermion_out-test8.h5")
+gauge = io.get_or_give(params, "_gauge-test8.h5")
+clover = io.get_or_give(params, "_clover-test8.h5")
+fermion_in = io.get_or_give(params, "_fermion_in-test8.h5")
+fermion_out = io.get_or_give(params, "_fermion_out-test8.h5")
 #############################
 gauge = cp.zeros_like(gauge)
 clover = cp.zeros_like(clover)
@@ -18,8 +18,8 @@ qcu.applyInitQcu(set_ptrs, params, argv)
 qcu.applyGaussGaugeQcu(gauge, set_ptrs, params)
 qcu.applyEndQcu(set_ptrs, params)
 #############################
-io.grid_xxxtzyx2hdf5_xxxtzyx(gauge, params, "gauge-test8.h5")
-io.grid_xxxtzyx2hdf5_xxxtzyx(clover, params, "clover-test8.h5")
-io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_in, params, "fermion_in-test8.h5")
-io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_out, params, "fermion_out-test8.h5")
+io.grid_xxxtzyx2hdf5_xxxtzyx(gauge, params, "_gauge-test8.h5")
+io.grid_xxxtzyx2hdf5_xxxtzyx(clover, params, "_clover-test8.h5")
+io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_in, params, "_fermion_in-test8.h5")
+io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_out, params, "_fermion_out-test8.h5")
 #############################
