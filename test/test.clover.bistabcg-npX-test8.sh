244077cc9a9d848259da6943022a73250b8ffe3e
zhangxin <zhangxin8069@qq.com>
2025-08-29 17:04:12 +0800

    2025-08-29-17-04-12

--- test/test.clover.bistabcg-npX-test8.sh
+++ test/test.clover.bistabcg-npX-test8.sh
@@ -3,9 +3,9 @@ pushd ../
 # bash ./install.sh
 popd
 rm _*
-# mpirun -x UCX_TLS=self,sm,rc -np 8 python -u ./test.gauge-test8.py >test.gauge-np8-test8.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 64 python -u ./test.clover.bistabcg-test8.py >test.clover.bistabcg-np64-test8.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 128 python -u ./test.clover.bistabcg-test8.py >test.clover.bistabcg-np128-test8.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 256 python -u ./test.clover.bistabcg-test8.py >test.clover.bistabcg-np256-test8.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 512 python -u ./test.clover.bistabcg-test8.py >test.clover.bistabcg-np512-test8.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 1024 python -u ./test.clover.bistabcg-test8.py >test.clover.bistabcg-np1024-test8.log 2>&1
+# mpirun -x UCX_TLS=self,sm,rc -np 8 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.gauge-test8.py > test.gauge-np8-test8.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 64 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test8.py > test.clover.bistabcg-np64-test8.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 128 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test8.py > test.clover.bistabcg-np128-test8.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 256 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test8.py > test.clover.bistabcg-np256-test8.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 512 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test8.py > test.clover.bistabcg-np512-test8.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 1024 "/public/home/zhangxin80699/openmpi_bind_mlnx.sh" python -u ./test.clover.bistabcg-test8.py > test.clover.bistabcg-np1024-test8.log 2>&1
