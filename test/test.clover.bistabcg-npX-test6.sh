55f73f749eb18ce946c9728d24a86baa1bed83c0
zhangxin <zhangxin8069@qq.com>
2025-08-29 13:46:33 +0800

    2025-08-29-13-46-33

--- test/test.clover.bistabcg-npX-test6.sh
+++ test/test.clover.bistabcg-npX-test6.sh
@@ -2,9 +2,9 @@ pushd ../
 # source ./env.sh
 # bash ./install.sh
 popd
-# mpirun -x UCX_TLS=self,sm,rc -np 1  python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np1-test6.log 2>&1
-# mpirun -x UCX_TLS=self,sm,rc -np 2 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np2-test6.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 4 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np4-test6.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 8 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np8-test6.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 1  python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np1-test6.log 2>&1
+mpirun -x UCX_TLS=self,sm,rc -np 2 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np2-test6.log 2>&1
+# mpirun -x UCX_TLS=self,sm,rc -np 4 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np4-test6.log 2>&1
+# mpirun -x UCX_TLS=self,sm,rc -np 8 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np8-test6.log 2>&1
 # mpirun -x UCX_TLS=self,sm,rc -np 16 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np16-test6.log 2>&1
 # mpirun -x UCX_TLS=self,sm,rc -np 32 python -u ./test.clover.bistabcg-test6.py >test.clover.bistabcg-np32-test6.log 2>&1
