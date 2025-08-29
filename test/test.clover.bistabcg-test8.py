5bc1bfa412a226842b8ce1e76d319146527a0c6c
zhangxin <zhangxin8069@qq.com>
2025-08-29 04:07:44 +0000

    2025-08-29-04-07-44

--- test/test.clover.bistabcg-test8.py
+++ test/test.clover.bistabcg-test8.py
@@ -4,10 +4,10 @@ from pyqcu.cuda import io
 from pyqcu.cuda import qcu
 from pyqcu.cuda.tmp_set import params, argv, set_ptrs
 #############################
-gauge = io.get_or_give(params, "gauge-test8.h5")
-clover = io.get_or_give(params, "clover-test8.h5")
-fermion_in = io.get_or_give(params, "fermion_in-test8.h5")
-_fermion_out = io.get_or_give(params, "fermion_out-test8.h5")
+gauge = io.get_or_give(params, "_gauge-test8.h5")
+clover = io.get_or_give(params, "_clover-test8.h5")
+fermion_in = io.get_or_give(params, "_fermion_in-test8.h5")
+_fermion_out = io.get_or_give(params, "_fermion_out-test8.h5")
 fermion_out = cp.zeros_like(_fermion_out)
 clover_ee = cp.zeros_like(clover)
 clover_ee_inv = cp.zeros_like(clover)
@@ -50,7 +50,7 @@ qcu.applyCloverBistabCgQcu(fermion_out, fermion_in,
                            gauge, clover_ee, clover_oo, clover_ee_inv, clover_oo_inv,  set_ptrs, params)
 qcu.applyEndQcu(set_ptrs, params)
 #############################
-io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_out, params, "fermion_out-test8.h5")
+io.grid_xxxtzyx2hdf5_xxxtzyx(fermion_out, params, "_fermion_out-test8.h5")
 #############################
 print(
     f"@cp.linalg.norm(fermion_out-_fermion_out)/cp.linalg.norm(fermion_out):{cp.linalg.norm(fermion_out-_fermion_out)/cp.linalg.norm(fermion_out)}@")
