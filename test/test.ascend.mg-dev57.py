51771017a57d3ddd353d49d91917ea7678bfedb3
zhangxin <zhangxin8069@qq.com>
2025-10-03 03:30:37 +0000

    2025-10-03-03-30-37

--- test/test.ascend.mg-dev57.py
+++ test/test.ascend.mg-dev57.py
@@ -3,9 +3,10 @@ import mpi4py.MPI as MPI
 from pyqcu.ascend import qcu
 lat_n = 8
 _qcu = qcu(lat_size=[lat_n, lat_n, lat_n, lat_n*2], dtype=torch.complex128,
-           device=torch.device('cpu'), dslash='clover', solver='bistabcg', verbose=False)
+           device=torch.device('cpu'), dslash='clover', solver='mg', verbose=False)
 _qcu.load(file_name='test.ascend-dev56')
 _qcu.init()
 _qcu.solve()
 _qcu.test()
+_qcu.full_mg.plot()
 # _qcu.save(file_name='test.ascend-dev56')
