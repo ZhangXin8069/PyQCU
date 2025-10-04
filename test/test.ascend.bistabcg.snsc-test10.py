dc5be7181cfab0a9544f641363a2cc4339598dd9
zhangxin <zhangxin8069@qq.com>
2025-10-04 16:50:04 +0000

    2025-10-04-16-50-04

--- test/test.ascend.bistabcg.snsc-test10.py
+++ test/test.ascend.bistabcg.snsc-test10.py
@@ -1,6 +1,6 @@
 from pyqcu.ascend import qcu
 import torch
-lat_n = 32
+lat_n = 16
 _qcu = qcu(lat_size=[lat_n, lat_n, lat_n, lat_n], dtype=torch.complex128,
            device=torch.device('cuda'), dslash='clover', solver='bistabcg', verbose=False)
 # _qcu.load()
--- test/test.ascend.mg.snsc-test10.py
+++ test/test.ascend.mg.snsc-test10.py
@@ -1,6 +1,6 @@
 from pyqcu.ascend import qcu
 import torch
-lat_n = 32
+lat_n = 16
 _qcu = qcu(lat_size=[lat_n, lat_n, lat_n, lat_n], dtype=torch.complex128,
            device=torch.device('cuda'), dslash='clover', solver='mg', verbose=False)
 _qcu.load()
