0401f8efe3114a33913650dfc8d658ea2e6d71bc
zhangxin <zhangxin8069@qq.com>
2025-10-04 16:43:49 +0000

    2025-10-04-16-43-49

--- test/test.ascend.bistabcg-test10.py
+++ test/test.ascend.bistabcg-test10.py
@@ -3,8 +3,8 @@ import torch
 lat_n = 8
 _qcu = qcu(lat_size=[lat_n, lat_n, lat_n, lat_n], dtype=torch.complex128,
            device=torch.device('cpu'), dslash='clover', solver='bistabcg', verbose=False)
-_qcu.load()
+# _qcu.load()
 _qcu.init()
 _qcu.solve()
 _qcu.test()
-# _qcu.save()
+_qcu.save()
