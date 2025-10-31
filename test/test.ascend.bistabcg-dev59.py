9ab72de2f927b4e913e0f97c813875768806e93a
zhangxin <zhangxin8069@qq.com>
2025-10-31 09:31:16 +0000

    2025-10-31-09-31-16

--- test/test.ascend.bistabcg-dev59.py
+++ test/test.ascend.bistabcg-dev59.py
@@ -7,7 +7,7 @@ lat_x, lat_y, lat_z, lat_t = 32, 32, 32, 32
 # lat_x, lat_y, lat_z, lat_t = 8, 8, 8, 8
 # lat_x, lat_y, lat_z, lat_t = 4, 4, 4, 4
 # lat_x, lat_y, lat_z, lat_t = 4, 4, 4, 8
-_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype=torch.complex128, device=torch.device(
+_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype=torch.complex64, device=torch.device(
     'npu'), dslash='clover', solver='bistabcg', verbose=True)
 _qcu.load()
 _qcu.init()
