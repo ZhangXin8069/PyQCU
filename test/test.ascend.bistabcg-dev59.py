7bb1deed795da3525a71ba25e60f6b7f733e35ca
zhangxin <zhangxin8069@qq.com>
2025-10-31 09:28:17 +0000

    2025-10-31-09-28-17

--- test/test.ascend.bistabcg-dev59.py
+++ test/test.ascend.bistabcg-dev59.py
@@ -1,11 +1,17 @@
 from pyqcu.ascend import qcu
 import torch
 lat_x, lat_y, lat_z, lat_t = 32, 32, 32, 32
-_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype=torch.complex64, device=torch.device(
-    'cuda'), dslash='wilson', solver='bistabcg', verbose=True)
-_qcu.load(file_name='quda_wilson_inverse_-32-32-32-32-f')
+# lat_x, lat_y, lat_z, lat_t = 16, 16, 16, 16
+# lat_x, lat_y, lat_z, lat_t = 16, 16, 16, 8
+# lat_x, lat_y, lat_z, lat_t = 16, 16, 8, 8
+# lat_x, lat_y, lat_z, lat_t = 8, 8, 8, 8
+# lat_x, lat_y, lat_z, lat_t = 4, 4, 4, 4
+# lat_x, lat_y, lat_z, lat_t = 4, 4, 4, 8
+_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype=torch.complex128, device=torch.device(
+    'npu'), dslash='clover', solver='bistabcg', verbose=True)
+_qcu.load()
 _qcu.init()
 _qcu.solve()
 _qcu.test()
 _qcu.mg.plot()
-_qcu.save(file_name='quda_wilson_inverse_-32-32-32-32-f')
+_qcu.save()
