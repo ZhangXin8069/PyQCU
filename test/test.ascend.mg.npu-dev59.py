3dcf5e7a60af51d0df588630c9788386807593e9
zhangxin <zhangxin8069@qq.com>
2025-11-02 15:12:34 +0000

    2025-11-02-15-12-34

--- test/test.ascend.mg.npu-dev59.py
+++ test/test.ascend.mg.npu-dev59.py
@@ -9,7 +9,7 @@ lat_x, lat_y, lat_z, lat_t = 16, 16, 16, 32
 # lat_x, lat_y, lat_z, lat_t = 4, 4, 4, 4
 # lat_x, lat_y, lat_z, lat_t = 4, 4, 8, 8
 _qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype_list=[torch.complex64, torch.complex64, torch.complex64, torch.complex32], device_list=[
-           torch.device('npu'), torch.device('npu'), torch.device('npu'), torch.device('npu')], dslash='clover', solver='mg', verbose=False)
+           torch.device('npu'), torch.device('npu'), torch.device('npu'), torch.device('npu')], dslash='clover', solver='mg', verbose=True)
 _qcu.load()
 _qcu.init()
 _qcu.solve()
