c5ac0d582a7b7f19b415205e92b6926763bd0519
zhangxin <zhangxin8069@qq.com>
2025-11-03 02:18:48 +0000

    2025-11-03-02-18-48

--- pyqcu/ascend/dslash.py
+++ pyqcu/ascend/dslash.py
@@ -556,8 +556,12 @@ class wilson_mg(wilson):
         name = dir_info['name']
         if self.verbose:
             print(f"@give_wilson_{name}_plus......")
-        print(f"src.shape: {src.shape}")
-        src_plus = torch_roll(src, shifts=-1, dims=axis)
+        try:
+            src_plus = torch_roll(src, shifts=-1, dims=axis)
+        except Exception as e:
+            print(f"src.shape: {src.shape}")
+            print(f"Error: {e}")
+            exit()
         if src_tail != None:
             src_plus[slice_dim(dim=5, ward=ward, point=-1)] = src_tail.clone()
         return torch_einsum(
