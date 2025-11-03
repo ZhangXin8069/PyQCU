869f1fb4fbc5401658eeeacaa0c4aacede18225a
zhangxin <zhangxin8069@qq.com>
2025-11-03 05:13:10 +0000

    2025-11-03-05-13-10

--- test/test.ascend.mg.npu-dev59.py
+++ test/test.ascend.mg.npu-dev59.py
@@ -1,7 +1,7 @@
 from pyqcu.ascend import qcu
 import torch
 lat_x, lat_y, lat_z, lat_t = 16, 16, 16, 32
-_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype_list=[torch.complex64, torch.complex32, torch.complex32, torch.complex32], device_list=[
+_qcu = qcu(lat_size=[lat_x, lat_y, lat_z, lat_t], dtype_list=[torch.complex64, torch.complex64, torch.complex64, torch.complex64], device_list=[
            torch.device('npu'), torch.device('npu'), torch.device('npu'), torch.device('npu')], dslash='clover', solver='mg', verbose=False)
 _qcu.load()
 _qcu.init()
