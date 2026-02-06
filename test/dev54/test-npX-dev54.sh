95ee163236898aaf13cfd73e2e037124e3b2b079
zhangxin <zhangxin8069@qq.com>
2026-02-06 21:22:13 +0000

    2026-02-06-21-22-13

--- test/dev54/test-npX-dev54.sh
+++
@@ -1,7 +0,0 @@
-command=$@
-echo "command:${command}"
-pushd ../
-# source ./env.sh
-# bash ./install.sh
-popd
-mpirun -x UCX_TLS=self,sm,rc -np ${command} '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.laplacian.py > .log 2>&1
--- test/dev59/test.ascend.mg.dcu-npX-dev59.sh
+++
@@ -1,9 +0,0 @@
-pushd ../
-# source ./env.sh
-# bash ./install.sh
-popd
-rm _*
-# mpirun -x UCX_TLS=self,sm,rc -np 1 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.ascend.mg.dcu-dev59.py > test.ascend.mg.dcu-np1-dev59.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 2 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.ascend.mg.dcu-dev59.py > test.ascend.mg.dcu-np2-dev59.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 4 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.ascend.mg.dcu-dev59.py > test.ascend.mg.dcu-np4-dev59.log 2>&1
-mpirun -x UCX_TLS=self,sm,rc -np 8 '/public/home/zhangxin80699/openmpi_bind_mlnx.sh' python -u ./test.ascend.mg.dcu-dev59.py > test.ascend.mg.dcu-np8-dev59.log 2>&1
