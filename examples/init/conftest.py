61ac2d2f7ebcf6e525a8bbad50225eae5e8c5edc
zhangxin <zhangxin8069@qq.com>
2026-02-27 02:24:59 +0000

    2026-02-27-02-24-59

--- examples/init/conftest.py
+++ examples/init/conftest.py
@@ -29,7 +29,15 @@ from pyqcu.testing import *
 #                    lat_size=[8, 8, 8, 8], support_parity=False)
 # test_solver(method='bistabcg', dtype=torch.complex128,
 #                    lat_size=[8, 8, 8, 8], support_parity=True)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=False)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=True)
+test_solver(method='bistabcg', dtype=torch.complex128,
+                   lat_size=[16, 16, 16, 32], support_parity=False)
+test_solver(method='bistabcg', dtype=torch.complex128,
+                   lat_size=[16, 16, 16, 32], support_parity=True)
 test_solver(method='multigrid', dtype=torch.complex128,
-                   lat_size=[8, 8, 8, 8], support_parity=False)
+                   lat_size=[16, 16, 16, 32], support_parity=False)
 test_solver(method='multigrid', dtype=torch.complex128,
-                   lat_size=[8, 8, 8, 8], support_parity=True)
+                   lat_size=[16, 16, 16, 32], support_parity=True)
