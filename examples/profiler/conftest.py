234d9e6d4ba3237e4bc7ff177559dc43a2feb406
zhangxin <zhangxin8069@qq.com>
2026-03-05 15:43:56 +0800

    2026-03-05-15-43-56

---
+++ examples/profiler/conftest.py
@@ -0,0 +1,67 @@
+from pyqcu.testing import *
+import cProfile
+import torch
+# test_import()
+# test_lattice()
+# test_dslash_wilson(with_data=True, suppoer_multi=True)
+# test_dslash_wilson(with_data=True, suppoer_multi=False)
+# test_dslash_wilson(with_data=False, suppoer_multi=False)
+# test_dslash_wilson(with_data=False)
+# test_dslash_clover()
+# test_dslash_parity()
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 8])
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 8], max_levels=1)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 8], max_levels=2)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 16, 16], max_levels=1)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 16, 16], max_levels=2)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 16], max_levels=1)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 16], max_levels=2, num_restart=1)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 16], max_levels=2, num_restart=3)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#             lat_size=[8, 8, 8, 16], max_levels=2, num_restart=5)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=False)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=True)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=False)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 8], support_parity=True)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 16, 16, 16], support_parity=False)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 16, 16, 16], support_parity=True)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 16, 16, 16], support_parity=False)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[8, 16, 16, 16], support_parity=True)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[16, 16, 16, 32], support_parity=False)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[16, 16, 16, 32], support_parity=True)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[16, 16, 16, 32], support_parity=False)
+# test_solver(method='multigrid', dtype=torch.complex128,
+#                    lat_size=[16, 16, 16, 32], support_parity=True)
+et_obs = torch.profiler.ExecutionTraceObserver()
+et_obs.register_callback(f"et_obs.json")
+# 创建 profile
+prof = torch.profiler.profile(
+activities=[torch.profiler.ProfilerActivity.CPU],  # 只用 CPU
+record_shapes=True,  # 记录 tensor shape
+execution_trace_observer=et_obs,
+with_stack=True,  # 记录调用栈，就能看到 trace
+)
+prof.start()
+test_solver(method='bistabcg', dtype=torch.complex128,
+                   lat_size=[8, 8, 8, 8], support_parity=True)
+prof.stop()
+prof.export_chrome_trace("trace.json")
