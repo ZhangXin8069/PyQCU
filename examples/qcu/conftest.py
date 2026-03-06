0852bf751047aa71f6706a1200e4e7f8c45b6191
zhangxin <zhangxin8069@qq.com>
2026-03-06 17:16:31 +0000

    2026-03-06-17-16-31

--- examples/qcu/conftest.py
+++ examples/qcu/conftest.py
@@ -50,5 +50,7 @@ import torch
 #                    lat_size=[16, 16, 16, 32], support_parity=False)
 # test_solver(method='multigrid', dtype=torch.complex128,
 #                    lat_size=[16, 16, 16, 32], support_parity=True)
-test_solver(method='bistabcg', dtype=torch.complex128,
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 16], support_parity=True)
+test_solver(method='multigrid', dtype=torch.complex128,device=torch.device('cuda'),
                    lat_size=[8, 8, 8, 16], support_parity=True)
