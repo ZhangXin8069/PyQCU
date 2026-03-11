4db7e2eac47d274ef6f24f25f974f1633e508ac5
zhangxin <zhangxin8069@qq.com>
2026-03-11 02:43:20 +0000

    2026-03-11-02-43-20

--- examples/profiler/conftest.py
+++ examples/profiler/conftest.py
@@ -66,8 +66,10 @@ prof = torch.profiler.profile(
     with_stack=True,
 )
 prof.start()
-test_solver(method='bistabcg', dtype=torch.complex128,
-                   lat_size=[8, 8, 8, 16], support_parity=True)
+# test_solver(method='bistabcg', dtype=torch.complex128,
+#                    lat_size=[8, 8, 8, 16], support_parity=True)
+test_solver(method='bistabcg', dtype=torch.complex64,device=torch.device('cuda'),
+                   lat_size=[8, 16, 16, 16], support_parity=True)
 prof.stop()
 prof.export_chrome_trace(
     f"{os.path.abspath(os.path.dirname(__file__))}/trace_{time}_{rank}.json")
