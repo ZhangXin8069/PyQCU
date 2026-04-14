9dd6c88deb3069b8639f00b9c2c19af59aae273c
zhangxin <zhangxin8069@qq.com>
2026-04-14 11:10:56 +0000

    2026-04-14-11-10-56

--- cpp/cuda/qcu/include/define.h
+++ cpp/cuda/qcu/include/define.h
@@ -175,8 +175,8 @@ namespace qcu {
 #define _GRID_EXAMPLE_ 1
 #define _MEM_POOL_ 0
 #define _CHECK_ERROR_ 1
-#define _TEST_SINGLE_IN_MULTI_ true
-#define _TEST_MULTI_IN_SINGLE_ true
+#define _TEST_SINGLE_IN_MULTI_ 1
+#define _TEST_MULTI_IN_SINGLE_ 0
 // cublas API error checking
 #define CUBLAS_CHECK(err)                                                      \
   do {                                                                         \
