7ebed6ace76c17d6846e15d717fb545b11cb4a47
zhangxin <zhangxin8069@qq.com>
2026-04-16 13:01:14 +0000

    2026-04-16-13-01-14

--- pyqcu/cuda/qcu.pyi
+++ pyqcu/cuda/qcu.pyi
@@ -4,140 +4,212 @@ from pyqcu.cuda import define
 
 def applyInitQcu(_set_ptrs: torch.Tensor = torch.Tensor([0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
                  _params: torch.Tensor = torch.Tensor(
-    [0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu')),
-    _argv: torch.Tensor = torch.Tensor([0.0]*define._SET_PTRS_SIZE_, dtype=define.dtype_half(define._LAT_C64_))) -> None: ...
+        [0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu')),
+        _argv: torch.Tensor = torch.Tensor([0.0]*define._SET_PTRS_SIZE_, dtype=define.dtype(define._LAT_C64_).to_real())) -> None:
+    """
+    follow above, most values in params should be set.
+    pay attention to the dtype of argv. (to_real)
+    """
+    ...
 
 
 def applyEndQcu(_set_ptrs: torch.Tensor = torch.Tensor([0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-                _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+                _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    """
+    ...
 
 
 def testWilsonDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                         _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyWilsonDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                          _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def testCloverDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                         _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyCloverDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                          _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyWilsonBistabCgQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                            _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyWilsonBistabCgDslashQcu(
-    _fermion_out: torch.Tensor = torch.Tensor(
-        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _fermion_in: torch.Tensor = torch.Tensor(
-        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        _fermion_out: torch.Tensor = torch.Tensor(
+            [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _fermion_in: torch.Tensor = torch.Tensor(
+            [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+            [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+            [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyWilsonCgQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                      _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyWilsonCgDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                            _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyLaplacianQcu(_laplacian_ou: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                       _laplacian_i: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    no even-odd, fermion_out:[scxyz1], fermion_in:[scxyz1] in [scxyz1], gauge:[ccdxyz1].
+    """
+    ...
 
 
 def applyCloverQcu(_clover: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, clover:[scscxyzt] in [pscscxyzt], gauge:[pccdxyzt].
+    """
 
 
 def applyCloversQcu(_clover: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-                    _clover_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+                    _clover_inv: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, clover:[scscxyzt] in [pscscxyzt], clover_inv:[scscxyzt] in [pscscxyzt], gauge:[pccdxyzt].
+    """
 
 
 def applyDslashQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                    _fermion_in: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _gauge: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _clover: torch.Tensor = torch.Tensor(
-    [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
-    _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _gauge: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _clover: torch.Tensor = torch.Tensor(
+        [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
+        _set_ptrs: torch.Tensor = torch.Tensor(
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], clover_inv:[scscxyzt] in [pscscxyzt], gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyGaussGaugeQcu(_gauge: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
                        _set_ptrs: torch.Tensor = torch.Tensor(
-    [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+        [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
+        _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None:
+    """
+    follow above, most values in params should be set.
+    even-odd, gauge:[pccdxyzt].
+    """
+    ...
 
 
 def applyCloverBistabCgQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
@@ -155,4 +227,9 @@ def applyCloverBistabCgQcu(_fermion_out: torch.Tensor = torch.Tensor([0.0]).to(d
     [0.0]).to(dtype=define.dtype(define._LAT_C64_), device=torch.device('cuda')),
     _set_ptrs: torch.Tensor = torch.Tensor(
     [0]*define._SET_PTRS_SIZE_).to(dtype=torch.int64, device=torch.device('cpu')),
-    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: ...
+    _params: torch.Tensor = torch.Tensor([0]*define._PARAMS_SIZE_).to(dtype=torch.int32, device=torch.device('cpu'))) -> None: 
+    """
+    follow above, most values in params should be set.
+    even-odd, parity in params should be set to 0 or 1, fermion_out:[scxyzt] in [pscxyzt], fermion_in:[scxyzt] in [pscxyzt], gauge:[pccdxyzt].
+    """
+    ...
