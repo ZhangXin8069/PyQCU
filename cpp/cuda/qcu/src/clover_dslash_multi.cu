698459147ea4064c08ba81cf1027e14afcf936fe
zhangxin <zhangxin8069@qq.com>
2026-04-16 11:22:12 +0000

    2026-04-16-11-22-12

--- cpp/cuda/qcu/src/clover_dslash_multi.cu
+++ cpp/cuda/qcu/src/clover_dslash_multi.cu
@@ -127,7 +127,7 @@ __global__ void make_clover_all(
     if_f_x = 0;
     if_f_y = 0;
     if_f_z = 0;
-    if_f_t = 0;
+    // if_f_t = 0;
     // if_b_x_b_y = 0;
     // if_b_x_b_z = 0;
     // if_b_x_b_t = 0;
