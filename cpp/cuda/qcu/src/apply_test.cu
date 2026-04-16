ccf4dcbed6e9c90c1f69ffd6600997fdb09c6de1
zhangxin <zhangxin8069@qq.com>
2026-04-16 11:09:09 +0000

    2026-04-16-11-09-09

--- cpp/cuda/qcu/src/apply_test.cu
+++ cpp/cuda/qcu/src/apply_test.cu
@@ -17,9 +17,7 @@ void testWilsonDslashQcu(long long _fermion_out, long long _fermion_in,
     // define for test_wilson_dslash
     LatticeSet<float> *set_ptr = static_cast<LatticeSet<float> *>(
         (void *)(static_cast<long long *>(set_ptrs)[set_index]));
-    
-    
-    
+
     auto start = std::chrono::high_resolution_clock::now();
     wilson_dslash<float><<<set_ptr->gridDim, set_ptr->blockDim>>>(
         gauge, fermion_in, fermion_out, set_ptr->device_params);
@@ -32,16 +30,12 @@ void testWilsonDslashQcu(long long _fermion_out, long long _fermion_in,
     printf("wilson dslash total time: (without malloc free memcpy) :%.9lf "
            "sec\n",
            double(duration) / 1e9);
-    
-    
-    
+
   } else if (data_type == _LAT_C128_) {
     // define for test_wilson_dslash
     LatticeSet<double> *set_ptr = static_cast<LatticeSet<double> *>(
         (void *)(static_cast<long long *>(set_ptrs)[set_index]));
-    
-    
-    
+
     auto start = std::chrono::high_resolution_clock::now();
     wilson_dslash<double><<<set_ptr->gridDim, set_ptr->blockDim>>>(
         gauge, fermion_in, fermion_out, set_ptr->device_params);
@@ -54,9 +48,7 @@ void testWilsonDslashQcu(long long _fermion_out, long long _fermion_in,
     printf("wilson dslash total time: (without malloc free memcpy) :%.9lf "
            "sec\n",
            double(duration) / 1e9);
-    
-    
-    
+
   } else {
     printf("data_type error\n");
   }
@@ -77,9 +69,7 @@ void testCloverDslashQcu(long long _fermion_out, long long _fermion_in,
     // define for test_clover_dslash
     LatticeSet<float> *set_ptr = static_cast<LatticeSet<float> *>(
         (void *)(static_cast<long long *>(set_ptrs)[set_index]));
-    
-    
-    
+
     LatticeWilsonDslash<float> _wilson_dslash;
     _wilson_dslash.give(set_ptr);
     void *clover;
@@ -146,9 +136,7 @@ void testCloverDslashQcu(long long _fermion_out, long long _fermion_in,
           "give clover total time: (without malloc free memcpy) :%.9lf sec\n ",
           double(duration) / 1e9);
     }
-    
-    
-    
+
     // free
     checkCudaErrors(cudaFreeAsync(clover, set_ptr->stream));
     checkCudaErrors(cudaStreamSynchronize(set_ptr->stream));
@@ -156,9 +144,7 @@ void testCloverDslashQcu(long long _fermion_out, long long _fermion_in,
     // define for test_clover_dslash
     LatticeSet<double> *set_ptr = static_cast<LatticeSet<double> *>(
         (void *)(static_cast<long long *>(set_ptrs)[set_index]));
-    
-    
-    
+
     LatticeWilsonDslash<double> _wilson_dslash;
     _wilson_dslash.give(set_ptr);
     void *clover;
@@ -225,9 +211,7 @@ void testCloverDslashQcu(long long _fermion_out, long long _fermion_in,
           "give clover total time: (without malloc free memcpy) :%.9lf sec\n ",
           double(duration) / 1e9);
     }
-    
-    
-    
+
     // free
     checkCudaErrors(cudaFreeAsync(clover, set_ptr->stream));
     checkCudaErrors(cudaStreamSynchronize(set_ptr->stream));
