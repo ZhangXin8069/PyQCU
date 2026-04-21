9e472ed6481f2ca3b60a3a1c2c809440e4808023
zhangxin <zhangxin8069@qq.com>
2026-04-21 07:42:16 +0000

    2026-04-21-07-42-16

--- cpp/cuda/qcu/src/clover_dslash_comm.cu
+++ cpp/cuda/qcu/src/clover_dslash_comm.cu
@@ -31,18 +31,18 @@ __global__ void pick_up_u_x(void *device_U, void *device_params,
       (static_cast<LatticeComplex<T> *>(device_u_b_x_send_vec) + idx);
   LatticeComplex<T> *u_f_x_send_vec =
       (static_cast<LatticeComplex<T> *>(device_u_f_x_send_vec) + idx);
-  for (int i = 0; i < _LAT_PCCD_; i++) {
-    int p = i / _LAT_CCD_;
-    int tmp = i - p * _LAT_CCD_;
-    int c0 = tmp / _LAT_CD_;
-    tmp -= c0 * _LAT_CD_;
-    int c1 = tmp / _LAT_D_;
-    tmp -= c1 * _LAT_D_;
-    int d = tmp;
-    printf("pick up "
-           "none:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-           idx, i, p, c0, c1, d, x, y, z, t);
-  }
+  // for (int i = 0; i < _LAT_PCCD_; i++) {
+  //   int p = i / _LAT_CCD_;
+  //   int tmp = i - p * _LAT_CCD_;
+  //   int c0 = tmp / _LAT_CD_;
+  //   tmp -= c0 * _LAT_CD_;
+  //   int c1 = tmp / _LAT_D_;
+  //   tmp -= c1 * _LAT_D_;
+  //   int d = tmp;
+  //   printf("pick up "
+  //          "none:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
+  //          idx, i, p, c0, c1, d, x, y, z, t);
+  // }
   // b_x
   tmp_U = (origin_U + ((((0) * lat_y + y) * lat_z + z) * lat_t + t));
   for (int i = 0; i < _LAT_PCCD_; i++) {
@@ -64,25 +64,25 @@ __global__ void pick_up_u_x(void *device_U, void *device_params,
            idx, i, p, c0, c1, d, x, y, z, t, tmp_U[i * lat_xyzt]._data.y);
   }
   // f_x
-  tmp_U = (origin_U + ((((lat_x - 1) * lat_y + y) * lat_z + z) * lat_t + t));
-  for (int i = 0; i < _LAT_PCCD_; i++) {
-    u_f_x_send_vec[i * lat_xyzt / lat_x] = tmp_U[i * lat_xyzt];
-    int p = i / _LAT_CCD_;
-    int tmp = i - p * _LAT_CCD_;
-    int c0 = tmp / _LAT_CD_;
-    tmp -= c0 * _LAT_CD_;
-    int c1 = tmp / _LAT_D_;
-    tmp -= c1 * _LAT_D_;
-    int d = tmp;
-    printf("pick up "
-           "f_x:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,tmp_U[i "
-           "* lat_xyzt]._data.x:%e\n",
-           idx, i, p, c0, c1, d, x, y, z, t, tmp_U[i * lat_xyzt]._data.x);
-    printf("pick up "
-           "f_x:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,tmp_U[i "
-           "* lat_xyzt]._data.y:%e\n",
-           idx, i, p, c0, c1, d, x, y, z, t, tmp_U[i * lat_xyzt]._data.y);
-  }
+  // tmp_U = (origin_U + ((((lat_x - 1) * lat_y + y) * lat_z + z) * lat_t + t));
+  // for (int i = 0; i < _LAT_PCCD_; i++) {
+  //   u_f_x_send_vec[i * lat_xyzt / lat_x] = tmp_U[i * lat_xyzt];
+  //   int p = i / _LAT_CCD_;
+  //   int tmp = i - p * _LAT_CCD_;
+  //   int c0 = tmp / _LAT_CD_;
+  //   tmp -= c0 * _LAT_CD_;
+  //   int c1 = tmp / _LAT_D_;
+  //   tmp -= c1 * _LAT_D_;
+  //   int d = tmp;
+  //   printf("pick up "
+  //          "f_x:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,tmp_U[i "
+  //          "* lat_xyzt]._data.x:%e\n",
+  //          idx, i, p, c0, c1, d, x, y, z, t, tmp_U[i * lat_xyzt]._data.x);
+  //   printf("pick up "
+  //          "f_x:idx,i,p,c0,c1,d,x,y,z,t:%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,tmp_U[i "
+  //          "* lat_xyzt]._data.y:%e\n",
+  //          idx, i, p, c0, c1, d, x, y, z, t, tmp_U[i * lat_xyzt]._data.y);
+  // }
 }
 template <typename T>
 __global__ void pick_up_u_y(void *device_U, void *device_params,
