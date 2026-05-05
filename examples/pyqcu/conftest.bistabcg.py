b9672ead2a3992a2fed35aae5e3aec210944409a
zhangxin <zhangxin8069@qq.com>
2026-05-05 16:10:22 +0000

    2026-05-05-16-10-22

--- examples/pyqcu/conftest.bistabcg.py
+++ examples/pyqcu/conftest.bistabcg.py
@@ -3,4 +3,4 @@ import torch
 mass = 0.05
 kappa = 1 / (2 * mass + 8)
 test_solver(method='multigrid', dtype=torch.complex64, device=torch.device('cuda'), kappa=torch.Tensor([kappa]),
-                   lat_size=[16, 16, 16, 32], max_level=4, support_parity=True)
+                   lat_size=[16, 16, 16, 32], max_level=1, support_parity=True)
