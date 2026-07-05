acdc7aadcf4fdecf9e24ae969c2d6b52bbfad367
zhangxin <zhangxin8069@qq.com>
2026-07-05 17:14:53 +0000

    2026-07-05-17-14-53

--- env.sh
+++ env.sh
@@ -1,6 +1,15 @@
-# @EXPORT@
-export LD_LIBRARY_PATH=${PWD}/cpp/cuda/qcu:$LD_LIBRARY_PATH
-export PYTHONPATH=${PWD}:${PYTHONPATH}
+#!/usr/bin/env bash
+_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+_NAME=$(basename "$0")
+echo "###${_NAME} in ${_PATH} is sourcing...:$(date "+%Y-%m-%d-%H-%M-%S")###"
+export LD_LIBRARY_PATH=${_PATH}/cpp/cuda/qcu:$LD_LIBRARY_PATH
+export PYTHONPATH=${_PATH}:${PYTHONPATH}
 export MPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
+
+
