9ec174c56e924942d7a7642e22a9a1ebd3919190
zhangxin <zhangxin8069@qq.com>
2026-08-14 17:35:08 +0800

    docs(conftest): 主入口注释清单补充 h5py 多线程测试 [opencode]

--- examples/pyqcu/conftest.py
+++ examples/pyqcu/conftest.py
@@ -1,5 +1,6 @@
 from pyqcu.testing import *
 import torch
+# test_h5py_multithread(nthreads=4, dtype=torch.complex64, lat_size=[4, 4, 4, 8])
 # test_import()
 # test_lattice()
 # test_dslash_wilson(with_data=True, support_parallel=True)
